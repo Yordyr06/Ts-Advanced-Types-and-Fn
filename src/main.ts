f1e900901f2d0523499d22d8a7c7d433cdb5ef33
Yordy Almonte <yoalmonte.dev@outlook.com>
2023-11-10 09:55:22 +0000

    More configs

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import './styles/input.css'
+import '../dist/output.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
