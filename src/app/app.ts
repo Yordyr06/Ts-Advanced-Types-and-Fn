aaef088bbc56c03e65465368f00df9f680ac21cf
Yordy Almonte <yoalmonte.dev@outlook.com>
2023-11-15 09:09:04 +0000

    Interface: Extends

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,2 +1,15 @@
-import { Product } from "./products/product.model";
-import { addProduct, products } from "./products/product.service";
+import { addProduct } from "./products/product.service";
+
+addProduct({
+  id: '1',
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  title: 'Product--01',
+  stock: 10,
+  category: {
+    id: '1',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    name: 'Category--01'
+  }
+})
