15e7eafcca1589d1343e1765e2341d58c92f28f5
Yordy Almonte <yoalmonte.dev@outlook.com>
2023-11-16 07:32:29 +0000

    CRUD example

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,28 @@
-import { addProduct } from "./products/product.service";
+import { faker } from '@faker-js/faker';
+import { addProduct, products} from "./products/product.service";
 
-addProduct({
-  id: '1',
-  createdAt: new Date(),
-  updatedAt: new Date(),
-  title: 'Product--01',
-  stock: 10,
-  category: {
-    id: '1',
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    name: 'Category--01'
-  }
-})
+for (let index = 0; index < 100; index++) {
+  addProduct({
+    id: faker.string.uuid(),
+    title: faker.commerce.productName(),
+    description: faker.commerce.productDescription(),
+    image: faker.image.url(),
+    size: faker.helpers.arrayElement(['S', 'M', 'L', 'XL']),
+    createdAt: faker.date.recent(),
+    updatedAt: faker.date.recent(),
+    stock: faker.number.int({min: 1, max: 50}),
+    color: faker.color.rgb({casing: 'mixed', format: 'hex', prefix: '#'}),
+    price: parseInt(faker.commerce.price({min: 1, max: 100}), 10),
+    category: {
+      id: faker.string.uuid(),
+      name: faker.commerce.department(),
+      createdAt: faker.date.recent(),
+      updatedAt: faker.date.recent(),
+    },
+    isNew: faker.datatype.boolean(),
+    tags: faker.datatype.array({min: 3, max: 5})
+    // tags: faker.helpers.arrayElements<string>([]),
+  })
+}
+
+console.log(products)
