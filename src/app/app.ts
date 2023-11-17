08a63624bb44ff297ef6d7e3bf780b605e40e9ef
Yordy Almonte <yoalmonte.dev@outlook.com>
2023-11-17 05:44:19 +0000

    Utility Types: Omit and Pick

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,22 +3,14 @@ import { addProduct, products} from "./products/product.service";
 
 for (let index = 0; index < 100; index++) {
   addProduct({
-    id: faker.string.uuid(),
     title: faker.commerce.productName(),
     description: faker.commerce.productDescription(),
     image: faker.image.url(),
     size: faker.helpers.arrayElement(['S', 'M', 'L', 'XL']),
-    createdAt: faker.date.recent(),
-    updatedAt: faker.date.recent(),
     stock: faker.number.int({min: 1, max: 50}),
     color: faker.color.rgb({casing: 'mixed', format: 'hex', prefix: '#'}),
     price: parseInt(faker.commerce.price({min: 1, max: 100}), 10),
-    category: {
-      id: faker.string.uuid(),
-      name: faker.commerce.department(),
-      createdAt: faker.date.recent(),
-      updatedAt: faker.date.recent(),
-    },
+    categoryId: faker.string.uuid(),
     isNew: faker.datatype.boolean(),
     tags: faker.datatype.array({min: 3, max: 5})
     // tags: faker.helpers.arrayElements<string>([]),
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,9 +1,24 @@
+import { faker } from "@faker-js/faker";
 import { Product } from "./product.model"
+import { CreateProduct } from "./product.dto";
 
 export const products: Product[] = [];
 
-export const addProduct = (data: Product) => {
-  products.push(data);
+export const addProduct = (data: CreateProduct): Product => {
+  const newProduct = {
+    ...data,
+    id: faker.string.uuid(),
+    createdAt: faker.date.recent(),
+    updatedAt: faker.date.recent(),
+    category: {
+      id: faker.string.uuid(),
+      name: faker.commerce.department(),
+      createdAt: faker.date.recent(),
+      updatedAt: faker.date.recent(),
+    }
+  }
+  products.push(newProduct);
+  return newProduct;
 }
 
 export const updateProduct = (id: string, changes: Product) => {
